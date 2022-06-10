a496c5863176497212ccf966574825f3ddf48c4e
wanglitong <wanglitong@bytedance.com>
2022-06-10 14:55:58 +0800

    feat: avoid pick repo when sharing same repo

--- src/git-remote/helper.ts
+++ src/git-remote/helper.ts
@@ -4,21 +4,21 @@ import { CommandRes, runCommand } from "./run-command";
 
 export type RemoteState = "ok" | "no_repo" | "fetch_error";
 
+export const checkGitRepo = async (workspace: WorkspaceFolder) => {
+  const path = workspace.uri.path;
+  const res = await runCommand("git status", path);
+  return res.ok;
+};
+
+export const getGitRemote = async (workspace: WorkspaceFolder) => {
+  const path = workspace.uri.path;
+  const remoteRes = await runCommand("git remote -v", path);
+  return remoteRes.ok ? remoteRes.res : null;
+};
+
 export const getGitRemoteInfo = async (
   workspace: WorkspaceFolder
 ): Promise<{ state: RemoteState; extra?: string }> => {
-  const path = workspace.uri.path;
-  const res = await runCommand("git status", path);
-  if (!res.ok) {
-    return { state: "no_repo" };
-  }
-  const remoteRes = await runCommand("git remote -v", path);
-  if (!remoteRes.ok) {
-    return { state: "no_repo" };
-  }
-  if (!remoteRes.res) {
-    return { state: "no_repo" };
-  }
   const updateRes = await updateRemote(workspace);
   if (!updateRes.ok) {
     return { state: "fetch_error", extra: updateRes.err.message };
--- src/workspace/index.ts
+++ src/workspace/index.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import { checkGitRepo, getGitRemote } from "../git-remote/helper";
 
 type WorkspaceChoice = {
   name: string;
@@ -45,6 +46,54 @@ export const pickWorkspace =
     if (spaceFolders.length === 1) {
       return spaceFolders[0].raw;
     }
-    const path = await quickPickWorkspace(spaceFolders);
+
+    const spaces = await getSpacesWithRepo(spaceFolders);
+    console.log(
+      "filter by repo",
+      spaces.map((x) => x.path)
+    );
+    if (spaces.length === 0) {
+      return null;
+    }
+    if (spaces.length === 1) {
+      return spaces[0].raw;
+    }
+    const path = await quickPickWorkspace(spaces);
     return spaceFolders.find((space) => space.path === path)?.raw || null;
   };
+
+const getSpacesWithRepo = async (
+  spaces: ReturnType<typeof getWorkspaceFolders>
+) => {
+  const ifWithRepo = spaces.map(async (space) => {
+    return {
+      ...space,
+      withRepo: await checkGitRepo(space.raw),
+    };
+  });
+
+  const spacesWithRepo = (await Promise.all(ifWithRepo)).filter((space) => {
+    return space.withRepo;
+  });
+  const withRemote = spacesWithRepo.map(async (space) => {
+    const remote = await getGitRemote(space.raw);
+    return {
+      ...space,
+      remote,
+    };
+  });
+  const res = (await Promise.all(withRemote)).filter((space) => {
+    return space.remote;
+  });
+  const remoteSet = new Set<string>();
+  return res.filter((space) => {
+    if (!space.remote) {
+      return false;
+    }
+    if (remoteSet.has(space.remote)) {
+      return false;
+    }
+    remoteSet.add(space.remote);
+    return true;
+  });
+};
